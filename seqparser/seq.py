6fd36f0305c6d517a87768a99e3583bef09ad512
Jessica Flowers <jess.flowers.m@gmail.com>
2026-01-14 11:40:03 -0800

    hw upload

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -9,14 +9,34 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     """
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
+
+    G --> C
+    C --> G
+    T --> A
+    A --> U
+
+    example:
+
+    input  : A C T G A A C C C
+         | | | | | | | | |
+    output : U G A C U U G G G
+
+
     """
-    pass
+    print('transcribing...')
+    mapping= {
+        'A':'U',
+        'T':'A', 
+        'C':'G', 
+        'G':'C'
+        }
+    trans_table = str.maketrans(mapping)
+    return seq.translate(trans_table)
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
+
     """
-    # Hey this is my comment
-    # Again!
-    pass
+    return transcribe(seq)[::-1]
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -31,17 +31,34 @@ def test_FastaParser():
 
     Some example of "good" test cases might be handling edge cases, like Fasta
     files that are blank or corrupted in some way. Two example Fasta files are
-    provided in /tests/bad.fa and /tests/empty.fa
+    provided in /tests/bad.fa and /tests/blank.fa
     """
-    pass
+    # test good example
+    parser_obj = FastaParser("data/test.fa")
+    file_lines = [record for record in parser_obj]
+    # known seq0 of fasta file
+    seq0 = 'TGATTGAATCTTTTGAGGGTCACGGCCCGGAAGCCAGAATTTCGGGGTCCTCTGTGGATATTAATCGAGCCCACACGGTGTGAGTTCAGCGGCCCCCGCA'
+    assert file_lines[0][1] == seq0, "Something is wrong - panic!"
 
+    # # test bad example
+    # parser_obj = FastaParser("data/test.fa")
+    # file_lines = [record for record in parser_obj]
+    # # known seq0 of fasta file
+    # seq0 = 'GATTGAATCTTTTGAGGGTCACGGCCCGGAAGCCAGAATTTCGGGGTCCTCTGTGGATATTAATCGAGCCCACACGGTGTGAGTTCAGCGGCCCCCGCA'
+    # assert file_lines[0][1] == seq0, "Something is wrong - panic!"
+
+    
 
 def test_FastaFormat():
     """
     Test to make sure that a fasta file is being read in if a fastq file is
     read, the first item is None
     """
-    pass
+    parser_obj = FastaParser("data/test.fa")
+    # parser_obj = FastaParser("data/test.fq")  # this should throw an error since wrong file type
+    file_lines = [record for record in parser_obj]    
+    assert file_lines[0][0] != None, "Ensure file is a FastA file."
+
 
 
 def test_FastqParser():
@@ -50,11 +67,50 @@ def test_FastqParser():
     an instance of your FastqParser class and assert that it properly reads 
     in the example Fastq File.
     """
-    pass
+    # good example
+    fastq_file = 'data/test.fq'
+    parser_obj = FastqParser(fastq_file)
+    file_lines = [record for record in parser_obj]
+
+    # Known seq0 of fastq file
+    seq0 = 'TGTGGTCGTATAGTTATTGTCATAAATTACACAGAATCGCGATTCTCCGCGTCCACCAATCTTAGTGCACCACAGCATCGACCCGATTTATGACGCTGAG'
+    # Known quality of seq0
+    qual0 = '*540($=*,=.062565,2>\'487\')!:&&6=,6,*7>:&132&83*8(58&59>\'8!;28<94,0*;*.94**:9+7"94(>7=\'(!5"2/!%"4#32='
+
+    # Check both sequence and quality
+    check_val = False
+    if ((file_lines[0][1] == seq0) and (file_lines[0][2] == qual0)):
+        check_val = True
+    
+    assert check_val, "FastQ parser error"
+    
+    # bad example
+    # # Read in fastq file
+    # fastq_file = 'data/test.fq'
+    # parser_obj = FastqParser(fastq_file)
+    # file_lines = [record for record in parser_obj]
+
+    # # Known seq0 of fastq file
+    # seq0 = 'TTGGTCGTATAGTTATTGTCATAAATTACACAGAATCGCGATTCTCCGCGTCCACCAATCTTAGTGCACCACAGCATCGACCCGATTTATGACGCTGAG'
+    # # Known quality of seq0
+    # qual0 = '*540($=*,=.062565,2>\'487\')!:&&6=,6,*7>:&132&83*8(58&59>\'8!;28<94,0*;*.94**:9+7"94(>7=\'(!5"2/!%"4#32='
+
+    # # Check both sequence and quality
+    # check_val = False
+    # if ((file_lines[0][1] == seq0) and (file_lines[0][2] == qual0)):
+    #     check_val = True
+    
+    # assert check_val, "FastQ parser error"
+
 
 def test_FastqFormat():
     """
     Test to make sure fastq file is being read in. If this is a fasta file, the
     first line is None
     """
-    pass
+    fastq_file = 'data/test.fq'
+    parser_obj = FastqParser(fastq_file)
+    # parser_obj = FastqParser("data/test.fa") # this should throw an error since wrong file type
+    file_lines = [record for record in parser_obj]
+    print(file_lines)
+    assert file_lines[0][0] != None, "Ensure file is a FastQ file."
